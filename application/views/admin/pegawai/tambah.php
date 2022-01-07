6dd4cb75aea96e1849346e80c9d1b48273261968
Willy Laurent Brenkman <laurentbrenkmanwilly@gmail.com>
2022-01-07 16:58:08 +0700

    Update tambah.php
    edit satu huruf

--- application/views/admin/pegawai/tambah.php
+++ application/views/admin/pegawai/tambah.php
@@ -42,7 +42,7 @@
     <div class="form-group">
         <label class="col-md-2 col-form-label">Tanggal lahir</label>
             <div class="col-md-5">
-                <input type="text" name="nama" class="form-control" placeholder="Tanggal Lahir value="<?php echo set_value('nama') ?>" required>
+                <input type="text" name="nama" class="form-control" placeholder="Tanggal Lahir" value="<?php echo set_value('nama') ?>" required>
             </div>
     </div>
     <div class="form-group">
