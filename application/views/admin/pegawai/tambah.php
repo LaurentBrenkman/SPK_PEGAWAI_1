5af5e1a058ee1dd94067953b41087d8961daceef
Willy Laurent Brenkman <laurentbrenkmanwilly@gmail.com>
2022-01-07 16:55:52 +0700

    revisi ke 10
    aku merubah tulisan nama nama datanya dari huruf kapital ke huruf kecil

--- application/views/admin/pegawai/tambah.php
+++ application/views/admin/pegawai/tambah.php
@@ -12,65 +12,65 @@
     echo form_open(base_url('admin/user/tambah'),' class="form-horizontal"');
     ?>
     <div class="form-group">
-        <label class="col-md-2 col-form-label">NIP</label>
+        <label class="col-md-2 col-form-label">Nip</label>
         <div class="col-md-5">
-            <input type="text" name="nama" class="form-control" placeholder="NIP" value="<?php echo set_value('nama') ?>" required>
+            <input type="text" name="nama" class="form-control" placeholder="Nip" value="<?php echo set_value('nama') ?>" required>
         </div>
     </div>
     <div class="form-group">
-        <label class="col-md-2 col-form-label">NAMA</label>
+        <label class="col-md-2 col-form-label">Nama</label>
         <div class="col-md-5">
-            <input type="text" name="nama" class="form-control" placeholder="NAMA PEGAWAI" value="<?php echo set_value('nama') ?>" required>
+            <input type="text" name="nama" class="form-control" placeholder="Nama Pegawai" value="<?php echo set_value('nama') ?>" required>
         </div>
     </div>
     <div class="form-group">
-        <label class="col-md-2 col-form-label">ALAMAT</label>
+        <label class="col-md-2 col-form-label">Alamat</label>
         <div class="col-md-5">
-            <input type="text" name="nama" class="form-control" placeholder="ALAMAT PEGAWAI" value="<?php echo set_value('nama') ?>" required>
+            <input type="text" name="nama" class="form-control" placeholder="Alamat Pegawai" value="<?php echo set_value('nama') ?>" required>
         </div>
     </div>
     <div class="form-group">
-        <label class="col-md-4 col-form-label">JENIS KELAMIN</label>
+        <label class="col-md-4 col-form-label">Jenis Kelamin</label>
             <div class="col-md-5">
                 <select class="form-control select2">
-                <option selected="selected">PILIH JENIS KELAMIN</option>
-                <option>LAKI - LAKI</option>
-                <option>PEREMPUAN</option>
+                <option selected="selected">- PILIH -</option>
+                <option>Laki - Laki</option>
+                <option>Perempuan</option>
                 </select>
             </div>
     </div>
     <div class="form-group">
-        <label class="col-md-2 col-form-label">TANGGAL LAHIR</label>
+        <label class="col-md-2 col-form-label">Tanggal lahir</label>
             <div class="col-md-5">
-                <input type="text" name="nama" class="form-control" placeholder="TMK" value="<?php echo set_value('nama') ?>" required>
+                <input type="text" name="nama" class="form-control" placeholder="Tanggal Lahir value="<?php echo set_value('nama') ?>" required>
             </div>
     </div>
     <div class="form-group">
-        <label class="col-md-2 col-form-label">TMK</label>
+        <label class="col-md-2 col-form-label">Tmk</label>
         <div class="col-md-5">
-            <input type="text" name="nama" class="form-control" placeholder="TMK" value="<?php echo set_value('nama') ?>" required>
+            <input type="text" name="nama" class="form-control" placeholder="Masukkan Tmk" value="<?php echo set_value('nama') ?>" required>
         </div>
     </div>
     <div class="form-group">
-        <label class="col-md-4 col-form-label">STATUS PEGAWAI</label>
+        <label class="col-md-4 col-form-label">Status Pegawai</label>
             <div class="col-md-5">
                 <select class="form-control select2">
-                <option selected="selected">PILIH STATUS PEGAWAI</option>
-                <option>TETAP</option>
-                <option>KONTRAK</option>
+                <option selected="selected">- Pilih -</option>
+                <option>Tetap</option>
+                <option>Kontrak</option>
                 </select>
             </div>
     </div>
     <div class="form-group">
-        <label class="col-md-2 col-form-label">PENDIDIKAN</label>
+        <label class="col-md-2 col-form-label">Pendidikan</label>
         <div class="col-md-5">
-            <input type="text" name="nama" class="form-control" placeholder="JENIS PENDIDIKAN" value="<?php echo set_value('nama') ?>" required>
+            <input type="text" name="nama" class="form-control" placeholder="Jenis Pendidikan" value="<?php echo set_value('nama') ?>" required>
         </div>
     </div>
     <div class="form-group">
-        <label class="col-md-2 col-form-label">GOLONGAN</label>
+        <label class="col-md-2 col-form-label">Golongan</label>
         <div class="col-md-5">
-            <input type="text" name="nama" class="form-control" placeholder="GOLONGAN PEGAWAI" value="<?php echo set_value('nama') ?>" required>
+            <input type="text" name="nama" class="form-control" placeholder="Golongan Pegawai" value="<?php echo set_value('nama') ?>" required>
         </div>
     </div>
     <?php echo form_close(); ?>
